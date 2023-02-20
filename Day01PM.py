2f3879fce773cdfeb281c79f7bbffc2e1631158b
raimondsivbulis@sylatech.com <raimondsivbulis@sylatech.com>
2023-02-20 14:21:06 +0000

    added some basic python

---
+++ Day01PM.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+print("hello world")
+
+# COMMAND ----------
+
+number1 = input("please enter a number")
+number2 = input("enter a second")
+
+print(int(number1) + int(number2))
+
+# COMMAND ----------
+
+
